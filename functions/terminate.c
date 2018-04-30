028e2f31d832bca349c911984b45829d0a0a2226
Joseph <26607983+JMillaCS@users.noreply.github.com>
2018-04-30 14:15:36 -0400

    Update terminate.c

--- functions/terminate.c
+++ functions/terminate.c
@@ -1,10 +1,28 @@
-void terminate(pid_t pid){
+#define _POSIX_C_SOURCE 200112L
+#define _ISOC99_SOURCE
+#define EXTENSIONS
+#define _XOPEN_SOURCE 600
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/types.h>
+#include <signal.h>
+
+
+void terminate(char *pid){
     int ret;
-    if (ret = kill(pid, SIGINT) == 0) {
-          deleteNodePid(pid);
+    ret = kill((atoi(pid)), SIGINT);
+    if (ret == 0) {
+          deleteNodePid((unsigned long)(atoi(pid)));
     }
      else{
        //kill() has returned -1
        fprintf(stderr,"kill() failed.\n");
      }
+}
+
+void main(char *pid){
+  terminate(pid);
 }
